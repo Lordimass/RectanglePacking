f42dfbcdc93cdb4c2ffabd896f0984272a696e19
Lordimass <sam@tchrin.net>
2024-05-02 19:39:57 +0100

    Preparation for integration with Lordimass.net

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import PIL.ImageDraw
 import numpy
 import os
 import math
+import json
 
 class Rect():
     def __init__(self, width, height=None, img_path:str=None, scale=1):
@@ -25,7 +26,7 @@ class Rect():
             random.randint(0,255),
         )
 class RectPacker():
-    def __init__(self, width, dir=None, rects=None):
+    def __init__(self, width, dir=None, rects=None, visualise = False):
         self.__sf = 1 # Everything must be scaled to this scale factor, then scaled back up to get real screen coordinates
         while width > 1000: # Forcing width to 3 figures to fight latency
             width //= 10
@@ -33,28 +34,34 @@ class RectPacker():
 
         self.space = (width, 10)
 
-        if rects != None and dir != None: # Both provided, pack both into space
+        self.rects = []
+        if rects != None: # Rects provided, pack rects into space
             self.rects = self.__sort_by_area(rects)
+        if dir != None: # Images provided, pack image dimensions into space
             self.__load_rects_from_images(dir)
-        elif rects != None: # Only rects provided, pack rects into space
-            self.rects = self.__sort_by_area(rects)
-        elif dir != None: # Only images provided, pack image dimensions into space
-            self.rects = []
-            self.__load_rects_from_images(dir)
-        else: # No source provided, raise an erro
+            self.__positions = {} # Dictionary of screen coordinates, saved to a file when done 
+        elif dir == None and rects == None: # No source provided, raise an error
             raise("No source for rectangles provided")
         
         self.__occupation = numpy.full(shape=(self.space[1], self.space[0]), fill_value=False)
-        self.__image = PIL.Image.new(mode="RGB", size=self.space)
-        self.__drawable = PIL.ImageDraw.ImageDraw(self.__image)
+        if visualise:
+            self.__image = PIL.Image.new(mode="RGB", size=self.space)
+            self.__drawable = PIL.ImageDraw.ImageDraw(self.__image)
+        self.visualise = visualise # Allows visualisation to be disabled for performance
 
     def pack(self): # Actual callable, main loop for packing.
         i = 0
         for rect in self.rects:
             self.__find_and_place(rect)
             i+=1
-        self.__image = self.__image.crop(self.__image.getbbox()) # Program intentionally overestimates height of the image, this crops it back down to size
-        self.__image.save("output.png")
+
+        if self.visualise:
+            self.__image = self.__image.crop(self.__image.getbbox()) # Program intentionally overestimates height of the image, this crops it back down to size
+            self.__image.save("output.png")
+
+        if self.__positions != {}: # If it handled images, save their positions to json
+            with open(f"positions.json", "w") as f:
+                json.dump(self.__positions, f, indent=4)
 
     def __load_rects_from_images(self, dir):
         img_paths = os.listdir(dir)
@@ -64,6 +71,7 @@ class RectPacker():
         maxres = 0
         for img_path in img_paths:
             img = Image.open(f"{dir}/{img_path}")
+            img.path = f"{dir}/{img_path}"
             imgs.append(img)
             maxres = max(maxres, img.width*img.height)
             
@@ -82,7 +90,7 @@ class RectPacker():
                 dim[1] = math.floor(dim[1] / 10)
                 sf *= 10
             self.rects.append(
-                Rect(dim[0], dim[1], img_path=img_path, scale=sf)
+                Rect(dim[0], dim[1], img_path=img.path, scale=sf)
             )
         
     def __sort_by_area(self, rectangles):
@@ -105,13 +113,24 @@ class RectPacker():
 
     def __place_rect(self, rect:Rect, pos:tuple):
         print(f"placing {rect.dim} at {pos}")
+
+        # Calculating corner coords
         upleft = pos
         downright = (pos[0]+rect.width-1, pos[1]+rect.height-1)
-        self.__drawable.rectangle(
-            [upleft, downright],
-            fill=rect.colour
-        )
+
+        # Draw rectangle on PIL
+        if self.visualise:
+            self.__drawable.rectangle(
+                [upleft, downright],
+                fill=rect.colour
+            )
+
+        # Update Occupation Array
         self.__occupation[upleft[1]:downright[1]+1, upleft[0]:downright[0]+1] = True
+
+        # Update positions dict if needed
+        if rect.img_path != None:
+            self.__positions[str(rect.img_path)] = (pos[0]*self.__sf, pos[1]*self.__sf)
 
     def __find_and_place(self, rect:Rect):
         for y in range(len(self.__occupation)):
@@ -151,13 +170,14 @@ class RectPacker():
         self.__occupation = numpy.concatenate((self.__occupation, additive))
 
         # Expanding image
-        newimage = PIL.Image.new(mode="RGB", size=(self.space[0], rect.height+self.__image.height))
-        newimage.paste(self.__image)
-        self.__image = newimage
-        self.__drawable = PIL.ImageDraw.ImageDraw(self.__image)
-        self.__find_and_place(rect)
+        if self.visualise:
+            newimage = PIL.Image.new(mode="RGB", size=(self.space[0], rect.height+self.__image.height))
+            newimage.paste(self.__image)
+            self.__image = newimage
+            self.__drawable = PIL.ImageDraw.ImageDraw(self.__image)
+            self.__find_and_place(rect)
 
-rects = [
+'''rects = [
     Rect(2,1),
     Rect(1,1),
     Rect(5,3),
@@ -170,6 +190,6 @@ rects = [
     Rect(1,2),
     Rect(6,2),
     Rect(6,6)
-]
+]'''
 
 RectPacker(width=1920, dir = "images").pack()
